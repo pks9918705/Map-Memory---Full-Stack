9318ba540ac65efe1abbde316c31678106dd002f
Priyanshu Kumar Singh <pks9918705@gmail.com>
2023-08-06 16:29:15 +0530

    Bug: Improve rating component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,29 @@ import "./App.css"
 import Map, { Marker, Popup } from 'react-map-gl';
 import StarIcon from '@mui/icons-material/Star';
 import axios from "axios"
-
-
+// import { format } from 'timeago.js';
+import moment from "moment"
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { useEffect, useState } from "react";
-// import { useState } from "react";
-
-// const MAPBOX_TOKEN = ''; // Set your mapbox token here
 
 function App() {
 
-  // const [showPopup,setShowPopup]= useState(true)
-  // console.log("**",showPopup)
+
+  const currentUser = "Priyadnshu22"
   const [pins, setPins] = useState([])
+  const [currentPlacedId, setCurrentPlacedId] = useState("")
+  const [newPlace, setNewPlace] = useState(null)
+  const [view, setView] = useState({
+    latitude: 37.8,
+    longitude: -122.4,
+    zoom: 14
+  })
+
+  // //? form ka data
+  const [place, setPlace] = useState("")
+  const [review, setReview] = useState("")
+  const [rating, setRating] = useState("")
+
 
   useEffect(() => {
 
@@ -24,36 +34,80 @@ function App() {
       try {
         const res = await axios.get('/pins')
         setPins(res.data)
-
-
+        // console.log(pins)
       }
       catch (err) { console.log(err) }
     }
     getPins()
-
   }, [])
-   
-   
+  useEffect(() => {
+    console.log("new pin", newPlace)
 
+  }, [newPlace])
+
+  useEffect(() => {
+    console.log("view change hua hai");
+  }, [view]);
+
+  const handleMarkerclick = (id, lat, long) => {
+
+    setCurrentPlacedId(id)
+    setView({ ...view, latitude: lat, longitude: long })
+
+  }
+  
+  const handleDoubleClick = (e) => {
+    console.log("double click -You clicked me", e)
+    const lng = e.lngLat.lng
+    const lat = e.lngLat.lat
+
+    console.log(lng,lat)
+    setNewPlace({ lng, lat })
+  
+    
+  }
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  
+    const newPin = {
+      username: currentUser,
+      title: place,
+      description: review,
+      rating: rating,
+      latitude: newPlace.lat,
+      longitude: newPlace.lng,
+    };
+  
+    console.log("Pin", newPin);
+  
+    try {
+      const res = await axios.post("/pins", newPin);
+      setPins((prevPins) => [...prevPins, res.data]); // Use functional update here
+      setNewPlace(null);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  
   return (
     <Map
-      initialViewState={{
-        latitude: 37.8,
-        longitude: -122.4,
-        zoom: 14
-      }}
+      initialViewState={view}
       style={{ width: "80vw", height: "100vh" }}
       mapStyle="mapbox://styles/mapbox/streets-v9"
       mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      onDblClick={handleDoubleClick}
+
     >
 
 
       {pins && pins.map(pin => (
-        <div div key={pin._id}>
-          <Marker longitude={pin.longitude} latitude={pin.latitude} color="red" />
+        <div key={pin._id}>
+          <Marker longitude={pin.longitude} latitude={pin.latitude} color="red" onClick={() => handleMarkerclick(pin._id, pin.latitude, pin.longitude)} />
 
-          <Popup longitude={pin.longitude} latitude={pin.latitude}
-            anchor="top-left" closeButton={true} closeOnClick={false}
+          {currentPlacedId === pin._id && <Popup longitude={pin.longitude} latitude={pin.latitude}
+            anchor="top-left" closeButton={true} closeOnClick={false} onClose={() => setCurrentPlacedId(null)}
 
           >
             <div className="card">
@@ -63,21 +117,51 @@ function App() {
               <p className='desc'>{pin.description}</p>
               <label htmlFor="">Rating</label>
               <div className='stars'>
-               
-                <StarIcon className='stars' />
-                <StarIcon className='stars' />
-                <StarIcon className='stars' />
-                <StarIcon className='stars' />
-                <StarIcon className='stars' />
+
+                {Array(pin.rating).fill(<StarIcon className='stars' />)}
               </div>
+              
               <label htmlFor="">Infromation</label>
               <span className='username'>Created by <b>{pin.username}</b></span>
-              <span className='date'>1 hour ago</span>
+              <span className='date'>{moment().format("MMM Do YY") }</span>
             </div>
-            You are here
-          </Popup>
+            {/* You are here */}
+          </Popup>}
         </div>
       ))}
+
+      {/* New Place */}
+ 
+      {newPlace && <Popup longitude={newPlace.lng} latitude={newPlace.lat}
+        anchor="top-left" closeButton={true} closeOnClick={false} onClose={() => setNewPlace(null)}
+
+      >
+        <form onSubmit={handleSubmit}>
+          <div className="card">
+            <h1 style={{ color: "tomato" }}> Add  a New Place</h1>
+            <label htmlFor="">Place</label>
+            <input placeholder="Enter Place Name" value={place} onChange={(e) => setPlace(e.target.value)} />
+            <label htmlFor="">Review</label>
+            <input placeholder="Enter Review" value={review} onChange={(e) => setReview(e.target.value)} />
+
+            <label htmlFor="">Rating</label>
+            <select>
+              <option value="1"  onChange={() => setRating(1)}>1</option>
+              <option value="2" onChange={() => setRating(2)}>2</option>
+              <option value="3" onChange={() => setRating(3)}>3</option>
+              <option value="4" onChange={() => setRating(4)}>4</option>
+              <option value="5" onChange={() => setRating(5)}>5</option>
+            </select>
+
+            <button className="btn">Add</button>
+
+           {/* <label htmlFor="">Infromation</label>
+          <span className='username'>Created by <b>kkkkbk</b></span>
+          <span className='date'>{format(new Date())}</span>   */}
+           </div>
+        </form>
+      </Popup>}  
+
     </Map >
   );
 }
